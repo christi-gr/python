a00c465ad09dbe792d055a212c461e3d9269ec53
christi-gr <99747973+christi-gr@users.noreply.github.com>
2022-03-27 21:17:58 +0400

    Delete 2.c

--- 2.c
+++
@@ -1,3 +0,0 @@
-1) write a python script to search .c file under a folder and its AA ZZ tree and save the AA ZZ paths. Input is the start folder AA ZZ. Output a txt file (file_list.txt)bbbb of the search result.
-2) write a python script which search from a set of files, AA ZZ texts by AA ZZ some text AA ZZ. Input is the above file (file_list.txt). Output is a csv file (AA ZZ_list.txt) AA ZZ to the text AA ZZ and its AA ZZ.
-3) write a python script which AA ZZ text in a file based on the AA ZZ_list.txt bbbb
