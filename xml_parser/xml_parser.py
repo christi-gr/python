3f5bb95fb47f61408df5b4e6067f3916806fc641
christi-gr <99747973+christi-gr@users.noreply.github.com>
2022-04-26 18:41:16 +0400

    Convert XML to CSV

---
+++ xml_parser/xml_parser.py
@@ -0,0 +1,35 @@
+import xml.etree.ElementTree as ET
+import re, os, glob
+import pandas as pd
+parser_file = '/tmp/sample.xml'
+
+def parse_file( parser_file ):
+    print(parser_file)
+    xml_p=open( parser_file,'r',  encoding="utf-8")
+    text = xml_p.read()
+    text = re.sub(r' xmlns="[^"]*"', '', text, count=1)
+    tree = ET.fromstring( text )
+    tags = ['PersonNm', 'TitleTxt', 'AverageHoursPerWeekRt', 'IndividualTrusteeOrDirectorInd', 'ReportableCompFromOrgAmt', 'ReportableCompFromRltdOrgAmt', 'OtherCompensationAmt']
+    cols = ['PersonNm', 'TitleTxt', 'AverageHoursPerWeekRt', 'IndividualTrusteeOrDirectorInd', 'ReportableCompFromOrgAmt', 'ReportableCompFromRltdOrgAmt', 'OtherCompensationAmt']
+
+    rows = []
+
+    #tree.find('//{http://www.irs.gov/efile}ReturnData/{http://www.irs.gov/efile}IRS990/{http://www.irs.gov/efile}Form990PartVIISectionAGrp')
+    for item in tree.findall('.//ReturnData/IRS990/Form990PartVIISectionAGrp'):
+        data = {}
+        for tag in tags:
+            data[ tag ] = ''
+            try:
+                data[ tag ] = item.find(tag).text
+            except:
+                pass
+        rows.append( data )
+    if ( len( rows ) ):
+        df = pd.DataFrame(rows, columns=cols)
+        output_file = os.path.basename( os.path.dirname(parser_file) ) + '_' + os.path.basename(parser_file).split('.')[0]
+        df.to_csv( './CSV/' + output_file + '.csv')
+
+
+files = glob.glob('**/*.xml', recursive=True)
+for f in files:
+    parse_file( f )
